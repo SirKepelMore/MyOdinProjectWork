76a37b41f3c50c34d82d320f33399fca9c3d4784
Stephen Morgan <83236570+SirKepelMore@users.noreply.github.com>
2021-07-03 18:14:47 -0400

    tempConversion
    My solution to the TOP tempConversion project.

---
+++ tempConversion/tempConversion.js
@@ -0,0 +1,17 @@
+const ftoc = function(f) {
+let c = (f - 32) * (5/9);
+c = Math.round(c * 10)/10;
+return c;
+};
+
+const ctof = function(c) {
+  let f = (c *(9/5)) + 32;
+  f = Math.round(f *10)/10;
+  return f;
+
+};
+
+module.exports = {
+  ftoc,
+  ctof
+};
